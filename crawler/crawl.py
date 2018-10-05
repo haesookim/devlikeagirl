f9b823065bc40a942dd0a602d92dd44ad05159d7
Ssuny76 <dayoungseo@seodayeong-ui-MacBook-Pro.local>
2018-10-05 17:21:24 +0900

    creator, artist modified

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -61,7 +61,7 @@ for i in range(1, 5):
                 
                 # genre selection
                 classCd = d.find_element_by_xpath('//*[@id="d_chart_search"]/div/div/div[5]/div[1]/ul/li[2]/span/label')
-                if '가요' not in classCd.text or '국내종합' not in classCd.text:
+                if '가요' not in classCd.text and '국내종합' not in classCd.text:
                     classCd = d.find_element_by_xpath('//*[@id="d_chart_search"]/div/div/div[5]/div[1]/ul/li/span/label')
                 classCd.click()
                 print(classCd.text)
@@ -88,39 +88,48 @@ for i in range(1, 5):
                         title = title.replace('19금', '')
 
                     title = re.sub('^\s*|\s+$','', title)
-
-                    artist = soup.find(attrs={"class": "artist_name"}).text
+                    
+                    singers = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find('div', class_ = "artist")))
 
                     album = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[0].text
                     date = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[1].text
-                    genre = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[2].text
-
-                    creator = re.sub('<[^>]*>|\s|\[|\]', ' ', str(soup.find_all("a", attrs={"class": "artist_name"})))
+                    genre = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[2].text 
+                    
+                    list_ = singers.split(',')
+                    count = len(list_)
+                    
+                    creator = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find_all("div", attrs={"class": "entry"})))
                     creator = re.sub('^\s*|\s+$', '', creator)
-
+                    clist = creator.split(',')
+                    creator = []
+                    for x in clist:
+                        if '작사' in x:
+                            creator.append(x[:-2])
+                    creator = ','.join(creator)        
                     lyric = re.sub('<[^>]*>|\s|\[|\]', ' ', str(soup.find_all(attrs={"class": "lyric"})[0]))
                     lyric = re.sub('^\s*|\s+$', '', lyric)
-
 
                     result.append({
                         'year': re.sub('[^0-9]', '', year.text),
                         'rank': rank.text,
                         'title': title,
-                        'artist': artist,
+                        'singers': singers,
                         'album': album,
                         'date' : date,
                         'genre': genre,
-                        'creator' : creator
+                        'creator' : creator,
+                        'lyric' : lyric
                         })
                     print("차트 연도:", year.text)
                     print("순위:", rank.text)
                     print("곡 id:", song_id)
                     print("제목:", title)
-                    print("아티스트:", artist)
+                    print("아티스트:", singers)
                     print("앨범:", album)
                     print("발매날짜:", date)
                     print("장르:", genre)
                     print("작사:", creator)
+                    print("가사:", lyric)
                     print("*_*_*_*_*_*_*_*_*_*_*__*_*_*")
                 with open('./data/melon_chart' + re.sub('[^0-9]', '', age.text) + 's.json', 'w', encoding='utf-8') as f:
                     j = json.dumps(result)
