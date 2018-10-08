d021e84db74ad519d68c59498a49c72a43cafb72
Ssuny76 <dayoungseo@seodayeong-ui-MacBook-Pro.local>
2018-10-08 20:08:30 +0900

    page change, file save modified

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -74,73 +74,91 @@ for i in range(1, 5):
                 d.find_element_by_xpath('//*[@id="d_srch_form"]/div[2]/button/span/span').click()
                 sleep(10)
 
-                song_ids = d.find_elements_by_xpath('//*[@id="lst50"]/td[4]/div/a')
-                song_ids = [re.sub('[^0-9]', '', song_id.get_attribute("href")) for song_id in song_ids]
-                ranks = d.find_elements_by_xpath('//*[@id="lst50"]/td[2]/div/span[1]')
+                # check whether next_page exists
+                next_exist = True
+                page_list = ['''"lst50"'''] # initialize with the first page
+                try:
+                    next_xpath = '//*[@id="frm"]/div[2]/span/a'
+                    next_button = d.find_element_by_xpath(next_xpath)
+                except: 
+                    next_exist = False
+                if next_exist:
+                    page_list.append('''"lst100"''') # next page
 
-                for rank, song_id in zip(ranks, song_ids):
-                    sleep(5)
-                    print(song_id)
+                for page in page_list:
+                    song_ids = d.find_elements_by_xpath('//*[@id={}]/td[4]/div/a'.format(page))
+                    song_ids = [re.sub('[^0-9]', '', song_id.get_attribute("href")) for song_id in song_ids]
+                    ranks = d.find_elements_by_xpath('//*[@id={}]/td[2]/div/span[1]'.format(page))
 
-                    req = requests.get('http://www.melon.com/song/detail.htm?songId=' + song_id, headers = header)
-                    html = req.text
-                    soup = BeautifulSoup(html, "html.parser")
+                    for rank, song_id in zip(ranks, song_ids):
+                        sleep(5)
+                        print(song_id)
 
-                    title = soup.find(attrs={"class": "song_name"}).text.replace('곡명', '')
+                        req = requests.get('http://www.melon.com/song/detail.htm?songId=' + song_id, headers = header)
+                        html = req.text
+                        soup = BeautifulSoup(html, "html.parser")
 
-                    if '19금' in title:
-                        title = title.replace('19금', '')
+                        title = soup.find(attrs={"class": "song_name"}).text.replace('곡명', '')
 
-                    title = re.sub('^\s*|\s+$','', title)
-                    
-                    # 가수
-                    singers = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find('div', class_ = "artist")))
+                        if '19금' in title:
+                            title = title.replace('19금', '')
 
-                    album = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[0].text
-                    date = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[1].text
-                    genre = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[2].text 
-                    
-                    # 작사가
-                    creator = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find_all("div", attrs={"class": "entry"})))
-                    creator = re.sub('^\s*|\s+$', '', creator)
-                    clist = creator.split(',')
-                    creator = []
-                    for x in clist:
-                        if '작사' in x:
-                            creator.append(x[:-2])
-                    creator = ','.join(creator)        
-                    
-                    # 가사
-                    lyric = re.sub('<[^>]*>|\s|\[|\]', ' ', str(soup.find_all(attrs={"class": "lyric"})[0]))
-                    lyric = re.sub('^\s*|\s+$', '', lyric)
-                    
-                    result.append({
-                        'year': re.sub('[^0-9]', '', year.text),
-                        'rank': rank.text,
-                        'title': title,
-                        'singers': singers,
-                        'album': album,
-                        'date' : date,
-                        'genre': genre,
-                        'creator' : creator,
-                        'lyric' : lyric
-                        })
-                    print("차트 연도:", year.text)
-                    print("순위:", rank.text)
-                    print("곡 id:", song_id)
-                    print("제목:", title)
-                    print("아티스트:", singers)
-                    print("앨범:", album)
-                    print("발매날짜:", date)
-                    print("장르:", genre)
-                    print("작사:", creator)
-                    print("가사:", lyric)
-                    print("*_*_*_*_*_*_*_*_*_*_*__*_*_*")
+                        title = re.sub('^\s*|\s+$','', title)
+                        
+                        # 가수
+                        singers = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find('div', class_ = "artist")))
 
-                with open('./result_{}.csv'.format(year.text), 'w') as csvfile:
-                    fieldnames = ["차트 연도", "순위", "곡 id", "제목", "아티스트", "앨범", "발매날짜", "장르", "작사", "가사"]
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                        album = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[0].text
+                        date = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[1].text
+                        genre = soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd')[2].text 
+                        
+                        # 작사가
+                        creator = re.sub('<[^>]*>|\s|\[|\]', '', str(soup.find_all("div", attrs={"class": "entry"})))
+                        creator = re.sub('^\s*|\s+$', '', creator)
+                        clist = creator.split(',')
+                        creator = []
+                        for x in clist:
+                            if '작사' in x:
+                                creator.append(x[:-2])
+                        creator = ','.join(creator)        
+                        
+                        # 가사
+                        lyric = re.sub('<[^>]*>|\s|\[|\]', ' ', str(soup.find_all(attrs={"class": "lyric"})[0]))
+                        lyric = re.sub('^\s*|\s+$', '', lyric)
+                        
+                        result.append({
+                            'time': re.sub('[^0-9~]', '.', year.text + week.text),
+                            'rank': rank.text,
+                            'song_id': song_id,
+                            'title': title,
+                            'singers': singers,
+                            'album': album,
+                            'date' : date,
+                            'genre': genre,
+                            'creator' : creator,
+                            'lyric' : lyric
+                            })
+                        print("차트 기간:", year.text +" "+ week.text)
+                        print("순위:", rank.text)
+                        print("곡 id:", song_id)
+                        print("제목:", title)
+                        print("가수:", singers)
+                        print("앨범:", album)
+                        print("발매날짜:", date)
+                        print("장르:", genre)
+                        print("작사:", creator)
+                        print("가사:", lyric)
+                        print("*_*_*_*_*_*_*_*_*_*_*__*_*_*")
 
-                    writer.writeheader()
-                    for i in len(result):
-                        writer.writerow(result[i])
+                    try:
+                        next_button.click()
+                    except:
+                        print("No next page")
+
+        with open('./result_{}.csv'.format(year.text), 'w') as csvfile:
+            fieldnames = ["time", "rank", "song_id", "title", "singers", "album", "date", "genre", "creator", "lyric"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for i in range(len(result)):
+                writer.writerow(result[0])
